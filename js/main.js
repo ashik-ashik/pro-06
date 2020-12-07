b6fced99800d091de9c8207a000ef08ecd497fe1
Ashik <kmdashik635@gmail.com>
2020-12-07 12:39:26 +0600

    add  go to top icon

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 
 $(document).ready(function(){
 
+    // gototop
+        $(window).scroll(function(){
+          if($(this).scrollTop()>200){
+              document.querySelector('.gototop').style.visibility = "visible";
+              $('.gototop').fadeIn('slow');
+          }else{
+              $('.gototop').fadeOut('slow'); 
+          }
+      });
+
+      $('.gocircle').click(function(){
+          $('html, body').animate({scrollTop:0}, 1000)
+      });
 
     // animation on Screen script
     function aos_init() {
