3c6cf51b8a9932bebf58e147c6eb39f7be06714a
Ashik <kmdashik635@gmail.com>
2020-12-07 10:32:48 +0600

    remove local files

--- js/main.js
+++ js/main.js
@@ -2,26 +2,6 @@
 $(document).ready(function(){
 
 
-
-  // client slider
-  $('#clientSlider').owlCarousel({
-    loop:true,
-    margin:25,
-    autoplay:true,
-    dots:true,
-    responsive:{
-      500:{
-        items:1,
-      },
-      767:{
-        items:3,
-      },
-      992:{
-        items:4,
-      }
-    }
-  })
-
     // animation on Screen script
     function aos_init() {
         AOS.init({
